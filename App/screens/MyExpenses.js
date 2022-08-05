c1b566f93511e91fca1569242908837f0e17b960
San Palban <palbanjessan15@gmail.com>
2022-08-05 16:57:21 +0800

    Stuck sa Data

--- App/screens/MyExpenses.js
+++ App/screens/MyExpenses.js
@@ -1,25 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import AppText from "../components/AppText";
 import Screen from "../components/Screen";
 import Header from "./Expenses/Header";
 import Icon from "../components/Icon";
-
+import colors from "../config/colors";
+import { VictoryPie } from "victory-native";
+import { DatabaseConnection } from "../components/Database/dbConnection";
+const db = DatabaseConnection.getConnection();
 function MyExpenses(props) {
+  const [getDate, setDate] = useState();
+  const [category, setCategory] = useState([]);
+  //Current Date to Display Default
+  const dateNow = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+  const sanDate = dateNow + "/" + (month + 1) + "/" + year;
+  useEffect(() => {
+    getDateNow();
+  }, []);
+
+  const getDateNow = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT category FROM table_income where date <= ? and date >= ?",
+        [sanDate, sanDate],
+
+        (tx, results) => {
+          const temp = [];
+          for (let i = 0; i < results.rows.length; ++i) {
+            temp.push(results.rows.item(i).category);
+            setCategory(temp);
+            tx.executeSql(
+              "SELECT SUM(amountBalance) as tot FROM table_income where category= ? and date <= ? and date >= ?",
+              [temp[i], sanDate, sanDate],
+              (tx2, results2) => {
+                for (let j = 0; j < results2.rows.length; ++j) {
+                  console.log(results2.rows.item(j).tot);
+                }
+              }
+            );
+          }
+        }
+      );
+    });
+  };
+  console.log(category);
+  const categoryToDisplay = () => {};
+
+  const renderChart = () => {
+    let chartData = categoryToDisplay();
+    return (
+      <View>
+        <VictoryPie data={chartData} />
+      </View>
+    );
+  };
   return (
     <>
       <Header />
-      <Screen style={{ backgroundColor: "yellow" }}>
+      <Screen>
         <View style={styles.graphContainer}>
-          <AppText>Categories</AppText>
-          <Icon name={"trash"} backgroundColor={"#fff"} iconColor={"red"} />
+          <AppText style={styles.textCat}>Categories</AppText>
+          <Icon
+            name={"list-ul"}
+            backgroundColor={"#fff"}
+            iconColor={colors.dark}
+            size={40}
+          />
         </View>
+        <View>{renderChart()}</View>
       </Screen>
     </>
   );
 }
 const styles = StyleSheet.create({
-  graphContainer: {},
+  graphContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textCat: {
+    fontSize: 20,
+  },
 });
 
 export default MyExpenses;
